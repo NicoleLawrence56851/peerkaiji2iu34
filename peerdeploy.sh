6581c704e4161c339b3e0d19eca85e7e15f17b7f
NicoleLawrence56851 <96326705+NicoleLawrence56851@users.noreply.github.com>
2022-03-22 17:19:00 -0400

    Update peerdeploy.sh

--- peerdeploy.sh
+++ peerdeploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-apt-get -y install tmux
-tmux
 wget https://raw.githubusercontent.com/NicoleLawrence56851/peer31fdg/main/peermain.sh
-chmod +x peer2fly.sh
-./peer2fly.sh --email lilnaxx9527@gmail.com --number 30
+chmod +x peermain.sh
+./peermain.sh --email lilnaxx9527@gmail.com --number 10
